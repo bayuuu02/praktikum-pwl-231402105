2c1e349e3f2ab994a5dd71ca324b8baca1c13c39
bayuuu02 <panciwarteg100@gmail.com>
2024-03-06 11:53:18 +0700

    first commit

---
+++ public/blog.js
@@ -0,0 +1,2 @@
+let nama = prompt("Selamat datang, nama kamu siapa?")
+alert("halo "+nama+", salam kenal!!!")
---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-3xl bg-green-500 font-bold underline">
+        bayu ajah!
+</h1>
+<div class="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>Dapatkan sepatu ini maka anda akan menjadi manusia terkeren sepanjang masaa!!!</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">BELI SEKARANG</button>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
